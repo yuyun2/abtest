3ce0e0a41e484f95e8a9f3783014f95001e5dddb
dongchris <cadong@usfca.edu>
2018-06-05 13:55:57 -0700

    add proportion plot

---
+++ Lab2_SampleSizeCalculator/proportion_sample_size.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+
+sample_size_proportion <- function(p1, delta, alpha, power){
+  p2 = p1 - delta
+  p1 = p2 + delta
+  
+  if(length(c(p1, delta, alpha, power))==4){
+  z_alpha_beta = (qnorm(alpha/2, lower.tail = F) + qnorm(1 - power, lower.tail = F))^2
+  n = round(z_alpha_beta * (p1*(1-p1) + p2*(1-p2)) / (p1-p2)^2)
+  print(paste("The sample size for a two-sample Z-test of proportions is", n))
+  }
+  
+  else{
+    
+    for(i in 1:4){
+      if(length(list(p1, delta, alpha, power)[[i]]) > 1){
+        multiple = i
+      }
+    }
+    
+    parameters = c("p1", "delta", "alpha", "power")
+    
+    print(paste("You have selected multiple values for", parameters[multiple], ":",
+                paste(list(p1, delta, alpha, power)[[multiple]], collapse = ", ")))
+    
+    n = c()
+    
+     if(parameters[multiple]=="p1"){
+      
+       
+      for(i in 1:length(p1)){
+        z_alpha_beta = (qnorm(alpha/2, lower.tail = F) + qnorm(1 - power, lower.tail = F))^2
+        n[i] = round(z_alpha_beta * (p1[i]*(1-p1[i]) + p2[i]*(1-p2[i])) / (p1[i]-p2[i])^2)
+       
+      print(paste("For p1 of", p1[i], "the sample size is", n[i]))
+       
+
+          
+      }
+       data = data_frame(n,p1)
+       ggplot(data, aes(x=p1, y=n)) + geom_line() +
+         ggtitle(paste('Sample Size vs Proportion A with alpha of',
+                       alpha, 'and power of', power)) +
+         xlab("Proportion A") + ylab("Sample Size (n)")
+       
+         
+    }
+    
+    
+    else if(parameters[multiple]=="delta"){
+      
+      for(i in 1:length(delta)){
+        z_alpha_beta = (qnorm(alpha/2, lower.tail = F) + qnorm(1 - power, lower.tail = F))^2
+        n[i] = round(z_alpha_beta * (p1[i]*(1-p1[i]) + p2[i]*(1-p2[i])) / (p1[i]-p2[i])^2)
+        print(paste("For delta of", p1[i] - p2[i], "the sample size is", n[i]))
+      }
+    }
+    
+    
+    else if(parameters[multiple]=="alpha"){
+      for(i in 1:length(alpha)){
+    z_alpha_beta = (qnorm(alpha[i]/2, lower.tail = F) + qnorm(1 - power, lower.tail = F))^2
+    n[i] = round(z_alpha_beta * (p1*(1-p1) + p2*(1-p2)) / (p1-p2)^2)
+    print(paste("For alpha of", alpha[i], ", the sample size is", n[i]))
+      }
+      
+      data = data_frame(n,alpha)
+      ggplot(data, aes(x=alpha, y=n)) + geom_line() +
+        ggtitle(paste('Sample Size vs alpha with Proportion A of',
+                      p1, 'and power of', power)) +
+        xlab("alpha") + ylab("Sample Size (n)")
+      
+    }
+    
+    
+    else if(parameters[multiple]=="power"){
+      for(i in 1:length(power)){
+        z_alpha_beta = (qnorm(alpha/2, lower.tail = F) + qnorm(1 - power[i], lower.tail = F))^2
+        n[i] = round(z_alpha_beta * (p1*(1-p1) + p2*(1-p2)) / (p1-p2)^2)
+        print(paste("For power of", power[i], "the sample size is", n[i]))
+      }
+    }
+    
+    
+    
+  
+  }
+}
+
+sample_size_proportion(p1 = 0.1,delta =  .05, alpha = .05, power = .8)
+sample_size_proportion(p1 = c(.05, .1),delta =  .05, alpha = .05, power = .8)
+sample_size_proportion(p1 = .1,delta =  c(.05,.1), alpha = .05, power = .8)
+sample_size_proportion(p1 = .1,delta =  .05, alpha = c(.05, .1), power = .8)
+sample_size_proportion(p1 = .1,delta =  .05, alpha = .05, power = c(.8,.9))
+
--- Lab2_SampleSizeCalculator/z_test_propotion.py
+++ Lab2_SampleSizeCalculator/z_test_propotion.py
@@ -1,14 +1,22 @@
 from scipy.stats import norm, zscore
 
-def sample_calculator_propotion(p1, p2, delta, power, sig):
+def sample_calculator_proportion(p1, delta, power, sig):
 
 	""" sample calculator of z test for propotion
 		input: pi1, pi2, effect size, power, significant level
 		output: sample size
 	"""
-    z_alpha = norm.isf([sig/2]) # two-sided 
-    z_oneMinusPower = norm.isf([1 - power]) 
-    s =(p1 * (1-p1) + p2 * (1-p2)) ** 2
-    n = s * ((z_alpha - z_oneMinusPower)**2) / (delta**2)
-    
-    return int(round(n[0]))   
+	p2 = delta - p1
+	print(p2)
+	z_alpha = norm.isf([sig/2]) # two-sided 
+	print(z_alpha)
+	z_oneMinusPower = norm.isf([1 - power]) 
+	print(z_oneMinusPower)
+	s =(p1 * (1-p1) + p2 * (1-p2)) ** 2
+	print(s)
+	n = s * ((z_alpha - z_oneMinusPower)**2) / (delta**2)
+	print(n)
+
+	return int(round(n[0]))
+
+print(sample_calculator_proportion(.1, .05, .8, .05))
